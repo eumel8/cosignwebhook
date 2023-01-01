3e1ddb9c326864bf0f53b68455b1450c64918696
Frank Kloeker <f.kloeker@telekom.de>
2023-01-01 17:55:00 +0100

    logging response

--- grumpy.go
+++ grumpy.go
@@ -65,7 +65,8 @@ func (gs *GrumpyServerHandler) serve(w http.ResponseWriter, r *http.Request) {
 		glog.Errorf("Can't encode response: %v", err)
 		http.Error(w, fmt.Sprintf("could not encode response: %v", err), http.StatusInternalServerError)
 	}
-	glog.Infof("Ready to write reponse ...")
+	//glog.Infof("Ready to write reponse ...")
+	glog.Infof("Ready to write reponse: ", resp)
 	if _, err := w.Write(resp); err != nil {
 		glog.Errorf("Can't write response: %v", err)
 		http.Error(w, fmt.Sprintf("could not write response: %v", err), http.StatusInternalServerError)
