89122d4653107eae139108358c9ddf4eefe33106
Bruno Bressi <bruno.bressi@telekom.de>
2023-12-14 23:24:52 +0100

    feat: split cleanup function and added event assert function

--- test/framework/client.go
+++ test/framework/client.go
@@ -50,14 +50,12 @@ func createClientSet() (k8sClient *kubernetes.Clientset, err error) {
 }
 
 // Cleanup removes all resources created by the framework
-// and cleans up the testing directory
-func (f *Framework) Cleanup(t testing.TB, err error) {
+// and cleans up the testing directory. If an error is passed,
+// the test will fail but the cleanup will still be executed.
+func (f *Framework) Cleanup(t testing.TB) {
 	cleanupKeys(t)
 	f.cleanupDeployments(t)
 	f.cleanupSecrets(t)
-	if err != nil {
-		t.Fatalf("test failed: %v", err)
-	}
 }
 
 // cleanupDeployments removes all deployments from the testing namespace
@@ -67,12 +65,15 @@ func (f *Framework) cleanupDeployments(t testing.TB) {
 	t.Logf("cleaning up deployments")
 	deployments, err := f.k8s.AppsV1().Deployments("test-cases").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
-		f.Cleanup(t, err)
+		f.Cleanup(t)
+		t.Fatal(err)
+
 	}
 	for _, d := range deployments.Items {
 		err = f.k8s.AppsV1().Deployments("test-cases").Delete(context.Background(), d.Name, metav1.DeleteOptions{})
 		if err != nil {
-			f.Cleanup(t, err)
+			f.Cleanup(t)
+			t.Fatal(err)
 		}
 	}
 
@@ -80,11 +81,12 @@ func (f *Framework) cleanupDeployments(t testing.TB) {
 	for {
 		select {
 		case <-timeout:
-			f.Cleanup(t, fmt.Errorf("timeout reached while waiting for deployments to be deleted"))
+			f.Cleanup(t)
 		default:
 			pods, err := f.k8s.CoreV1().Pods("test-cases").List(context.Background(), metav1.ListOptions{})
 			if err != nil {
-				f.Cleanup(t, err)
+				f.Cleanup(t)
+				t.Fatal(err)
 			}
 
 			if len(pods.Items) == 0 {
@@ -102,7 +104,8 @@ func (f *Framework) cleanupSecrets(t testing.TB) {
 	t.Logf("cleaning up secrets")
 	secrets, err := f.k8s.CoreV1().Secrets("test-cases").List(context.Background(), metav1.ListOptions{})
 	if err != nil {
-		f.Cleanup(t, err)
+		f.Cleanup(t)
+		t.Fatal(err)
 	}
 	if len(secrets.Items) == 0 {
 		return
@@ -110,7 +113,8 @@ func (f *Framework) cleanupSecrets(t testing.TB) {
 	for _, s := range secrets.Items {
 		err = f.k8s.CoreV1().Secrets("test-cases").Delete(context.Background(), s.Name, metav1.DeleteOptions{})
 		if err != nil {
-			f.Cleanup(t, err)
+			f.Cleanup(t)
+			t.Fatal(err)
 		}
 	}
 }
@@ -119,7 +123,8 @@ func (f *Framework) cleanupSecrets(t testing.TB) {
 func (f *Framework) CreateDeployment(t testing.TB, d appsv1.Deployment) {
 	_, err := f.k8s.AppsV1().Deployments("test-cases").Create(context.Background(), &d, metav1.CreateOptions{})
 	if err != nil {
-		f.Cleanup(t, err)
+		f.Cleanup(t)
+		t.Fatal(err)
 	}
 }
 
@@ -133,14 +138,16 @@ func (f *Framework) WaitForDeployment(t *testing.T, d appsv1.Deployment) {
 	})
 
 	if err != nil {
-		f.Cleanup(t, err)
+		f.Cleanup(t)
+		t.Fatal(err)
 	}
 
 	timeout := time.After(30 * time.Second)
 	for event := range w.ResultChan() {
 		select {
 		case <-timeout:
-			f.Cleanup(t, fmt.Errorf("timeout reached while waiting for deployment to be ready"))
+			f.Cleanup(t)
+			t.Fatal("timeout reached while waiting for deployment to be ready")
 		default:
 			deployment, ok := event.Object.(*appsv1.Deployment)
 			if !ok {
@@ -156,7 +163,8 @@ func (f *Framework) WaitForDeployment(t *testing.T, d appsv1.Deployment) {
 		}
 	}
 
-	f.Cleanup(t, fmt.Errorf("failed to wait for deployment to be ready"))
+	f.Cleanup(t)
+	t.Fatal("failed to wait for deployment to be ready")
 }
 
 // CreateSecret creates a secret in the testing namespace
@@ -164,7 +172,8 @@ func (f *Framework) CreateSecret(t *testing.T, secret corev1.Secret) {
 	t.Logf("creating secret %s", secret.Name)
 	s, err := f.k8s.CoreV1().Secrets("test-cases").Create(context.Background(), &secret, metav1.CreateOptions{})
 	if err != nil {
-		f.Cleanup(t, err)
+		f.Cleanup(t)
+		t.Fatal(err)
 	}
 	t.Logf("created secret %s", s.Name)
 }
@@ -177,7 +186,8 @@ func (f *Framework) AssertDeploymentFailed(t *testing.T, d appsv1.Deployment) {
 	// watch for replicasets of the deployment
 	rsName, err := f.waitForReplicaSetCreation(t, d)
 	if err != nil {
-		f.Cleanup(t, err)
+		f.Cleanup(t)
+		t.Fatal(err)
 	}
 
 	// get warning events of deployment's namespace and check if the deployment failed
@@ -185,14 +195,16 @@ func (f *Framework) AssertDeploymentFailed(t *testing.T, d appsv1.Deployment) {
 		FieldSelector: fmt.Sprintf("involvedObject.name=%s", rsName),
 	})
 	if err != nil {
-		f.Cleanup(t, err)
+		f.Cleanup(t)
+		t.Fatal(err)
 	}
 
 	timeout := time.After(30 * time.Second)
 	for event := range w.ResultChan() {
 		select {
 		case <-timeout:
-			f.Cleanup(t, fmt.Errorf("timeout reached while waiting for deployment to fail"))
+			f.Cleanup(t)
+			t.Fatal("timeout reached while waiting for deployment to fail")
 		default:
 			e, ok := event.Object.(*corev1.Event)
 			if !ok {
@@ -208,12 +220,48 @@ func (f *Framework) AssertDeploymentFailed(t *testing.T, d appsv1.Deployment) {
 	}
 }
 
+// AssertEventForPod asserts that a PodVerified event is created
+func (f *Framework) AssertEventForPod(t *testing.T, reason string, p corev1.Pod) {
+
+	t.Logf("waiting for %s event to be created for pod %s", reason, p.Name)
+
+	// watch for events of deployment's namespace and check if the podverified event is created
+	w, err := f.k8s.CoreV1().Events("test-cases").Watch(context.Background(), metav1.ListOptions{
+		FieldSelector: fmt.Sprintf("involvedObject.name=%s", p.Name),
+	})
+	if err != nil {
+		f.Cleanup(t)
+		t.Fatal(err)
+	}
+
+	timeout := time.After(30 * time.Second)
+	for event := range w.ResultChan() {
+		select {
+		case <-timeout:
+			f.Cleanup(t)
+			t.Fatal("timeout reached while waiting for podverified event")
+		default:
+			e, ok := event.Object.(*corev1.Event)
+			if !ok {
+				time.Sleep(5 * time.Second)
+				continue
+			}
+			if e.Reason == reason {
+				t.Logf("%s event created for pod %s", reason, p.Name)
+				return
+			}
+			time.Sleep(5 * time.Second)
+		}
+	}
+}
+
 func (f *Framework) waitForReplicaSetCreation(t *testing.T, d appsv1.Deployment) (string, error) {
 	rs, err := f.k8s.AppsV1().ReplicaSets("test-cases").Watch(context.Background(), metav1.ListOptions{
 		LabelSelector: fmt.Sprintf("app=%s", d.Name),
 	})
 	if err != nil {
-		f.Cleanup(t, err)
+		f.Cleanup(t)
+		t.Fatal(err)
 	}
 
 	timeout := time.After(30 * time.Second)
@@ -231,4 +279,17 @@ func (f *Framework) waitForReplicaSetCreation(t *testing.T, d appsv1.Deployment)
 		}
 	}
 	return "", fmt.Errorf("failed to wait for replicaset creation")
+}
+
+// GetPods returns the pod(s) of the deployment. The fetch is done by label selector (app=<deployment name>)
+// If the get request fails, the test will fail and the framework will be cleaned up
+func (f *Framework) GetPods(t *testing.T, d appsv1.Deployment) *corev1.PodList {
+	pods, err := f.k8s.CoreV1().Pods("test-cases").List(context.Background(), metav1.ListOptions{
+		LabelSelector: fmt.Sprintf("app=%s", d.Name),
+	})
+	if err != nil {
+		f.Cleanup(t)
+		t.Fatal(err)
+	}
+	return pods
 }
